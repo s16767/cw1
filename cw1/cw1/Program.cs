ffe0058de2c518397c337c9dbf11f53acbc3430d
Mateusz Świerczewski <s16767@pjwstk.edu.pl>
2020-03-01 14:27:12 +0100

    Napisalem program

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+            }
+
+            Console.WriteLine("Koniec!");
         }
-    }
+    } 
 }
